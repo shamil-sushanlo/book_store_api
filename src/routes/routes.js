c8386f2e84b8e89d89d200c51de62bfbbdf317f8
Shamil Sushanlo <shamil.kg26@gmail.com>
2024-08-26 03:10:58 +0300

    API for book-store, day 1

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+
+const router = express.Router()
+
+router.get('/books',)
+router.get('/books/id',)
+router.post('books',)
+
+export default router
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import http from 'http'
+import mongoose from 'mongoose'
+import app from './app.js'
+import dotenv from "dotenv";
+
+dotenv.config({ path: '../.env' });
+
+const host = "192.168.0.122";
+const mongoURI = `mongodb://${username}:${password}@${hostdb}:${port}/${dbName}?authSource=admin`;
+const PORT = process.env.PORT || 3000
+const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const hostdb = process.env.MONGO_HOST;
+const port = process.env.MONGO_PORT;
+const dbName = process.env.MONGO_DB_NAME;
+
+mongoose.connect(mongoURI).then(()=>{
+    console.log('connected to MongoDB')
+    const server = http.createServer(app)
+
+    server.listen(PORT, host, ()=>{
+        console.log('server is running')
+    })
+}).catch(err => {
+    console.log('not')
+})
