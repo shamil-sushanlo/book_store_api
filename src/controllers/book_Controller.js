9b577d64518d843a6a960b64a2d846a65a8f8c4f
Shamil Sushanlo <shamil.kg26@gmail.com>
2024-08-30 04:21:06 +0300

    add Schema and Model

--- src/controllers/book_Controller.js
+++ src/controllers/book_Controller.js
@@ -1,8 +1,21 @@
-function getBook(req, res, next){
-    try {
-      res.send('hello world')
-    } catch (err) {
-      console.log(err);
-    }
-  };
+import Book from "../models/book.js";
+
+async function run() {
+  const book = new Book({
+    name: "Преступление и накозание",
+    author: "Фёдор Достоевский",
+    publication_year: 1866,
+  });
+
+  await book.save();
+}
+run();
+
+function getBook(req, res, next) {
+  try {
+    res.send("hello world");
+  } catch (err) {
+    console.log(err);
+  }
+}
 export default getBook;
